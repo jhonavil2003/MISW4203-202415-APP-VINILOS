e1ca835165b15c8546cf14aa3f11464de2ac81ae
Jairo Reyes <ja.reyesr1@uniandes.edu.co>
2024-10-30 00:46:29 -0500

    Se agrega lógica en fragmento y view model para cargar y mostrar información de albumes

---
+++ app/src/main/java/com/example/app_vinilos_g17/ui/albums/AlbumFragment.kt
@@ -0,0 +1,69 @@
+package com.example.app_vinilos_g17.ui.albums
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.app_vinilos_g17.ui.adapters.AlbumAdapter
+import com.example.app_vinilos_g17.databinding.FragmentAlbumBinding
+
+class AlbumFragment : Fragment() {
+
+    private var _binding: FragmentAlbumBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: AlbumViewModel by viewModels { AlbumViewModel.Factory(requireActivity().application) }
+    private lateinit var viewModelAdapter: AlbumAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModelAdapter = AlbumAdapter()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.albumsRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = viewModelAdapter
+        }
+
+        viewModel.albums.observe(viewLifecycleOwner, Observer { albums ->
+            albums?.let {
+                viewModelAdapter.albums = it // Actualiza el adaptador con los nuevos datos
+            }
+        })
+
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+            binding.albumsRv.visibility = if (isLoading) View.GONE else View.VISIBLE // Oculta el RecyclerView mientras carga
+        })
+
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError() {
+        if (!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
+    }
+}
