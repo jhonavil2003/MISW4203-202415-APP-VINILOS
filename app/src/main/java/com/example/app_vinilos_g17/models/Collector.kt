e72d044c348f1bd2e9b180f535336751d967970d
Andrea Fabiana Villamizar Ruiz <af.villamizarr1@uniandes.edu.co>
2024-11-27 20:24:15 -0500

    Se agg atributos al modelo Collector para el funcionamiento de CollectorDetail

--- app/src/main/java/com/example/app_vinilos_g17/models/Collector.kt
+++ app/src/main/java/com/example/app_vinilos_g17/models/Collector.kt
@@ -1,8 +1,11 @@
 package com.example.app_vinilos_g17.models
 
-data class Collector (
+data class Collector(
     val collectorId: Int,
-    val name:String,
-    val telephone:String,
-    val email:String
+    val name: String,
+    val telephone: String,
+    val email: String,
+    val comments: List<Comment> = emptyList(),
+    val favoritePerformers: List<Performer> = emptyList(),
+    val collectorAlbums: List<CollectorAlbum> = emptyList()
 )
---
+++ app/src/main/java/com/example/app_vinilos_g17/models/CollectorAlbum.kt
@@ -0,0 +1,7 @@
+package com.example.app_vinilos_g17.models
+
+data class CollectorAlbum(
+    val id: Int,
+    val price: Int,
+    val status: String
+)
