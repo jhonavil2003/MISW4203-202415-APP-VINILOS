9f2e7d8eadb95fffcc7d35ea888224a31331e2b4
Andrea Fabiana Villamizar Ruiz <af.villamizarr1@uniandes.edu.co>
2024-11-28 15:13:37 -0500

    se agg información en la vista de detalle

--- app/src/main/java/com/example/app_vinilos_g17/ui/view/CollectorDetailFragment.kt
+++ app/src/main/java/com/example/app_vinilos_g17/ui/view/CollectorDetailFragment.kt
@@ -9,7 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.app_vinilos_g17.databinding.FragmentCollectorDetailBinding
+import com.example.app_vinilos_g17.ui.adapters.CommentAdapter
+import com.example.app_vinilos_g17.ui.adapters.PerformerAdapter
 import com.example.app_vinilos_g17.viewmodels.CollectorDetailViewModel
 
 class CollectorDetailFragment : Fragment() {
@@ -19,6 +22,8 @@ class CollectorDetailFragment : Fragment() {
 
     private val args: CollectorDetailFragmentArgs by navArgs()
     private lateinit var viewModel: CollectorDetailViewModel
+    private lateinit var performerAdapter: PerformerAdapter
+    private lateinit var commentAdapter: CommentAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,20 +36,50 @@ class CollectorDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val collectorId = args.collectorId
-        viewModel = ViewModelProvider(this, CollectorDetailViewModel.Factory(requireActivity().application, collectorId)).get(
-            CollectorDetailViewModel::class.java)
+        viewModel = ViewModelProvider(
+            this,
+            CollectorDetailViewModel.Factory(requireActivity().application, collectorId)
+        ).get(CollectorDetailViewModel::class.java)
 
-        viewModel.collectorDetail.observe(viewLifecycleOwner) { collector ->
-            binding.collectorName.text = collector.name
-            binding.collectorTelephone.text = collector.telephone
-            binding.collectorEmail.text = collector.email
+        binding.recyclerViewPerformers.layoutManager = LinearLayoutManager(context)
+        performerAdapter = PerformerAdapter()
+        binding.recyclerViewPerformers.adapter = performerAdapter
+
+
+        binding.recyclerViewComments.layoutManager = LinearLayoutManager(context)
+        commentAdapter = CommentAdapter(emptyList())
+        binding.recyclerViewComments.adapter = commentAdapter
+
+        viewModel.collector.observe(viewLifecycleOwner) { collector ->
+            binding.collector = collector
             (activity as? AppCompatActivity)?.supportActionBar?.title = collector.name
+
+            performerAdapter.updatePerformers(collector.favoritePerformers)
+
+//            if (collector.albums.isEmpty()) {
+//                binding.textViewNoAlbums.visibility = View.VISIBLE
+//                binding.recyclerViewAlbums.visibility = View.GONE
+//            } else {
+//                binding.textViewNoAlbums.visibility = View.GONE
+//                binding.recyclerViewAlbums.visibility = View.VISIBLE
+//                albumAdapter.updateAlbums(collector.albums)
+//            }
+
+            if (collector.comments.isEmpty()) {
+                binding.textViewNoComments.visibility = View.VISIBLE
+                binding.recyclerViewComments.visibility = View.GONE
+            } else {
+                binding.textViewNoComments.visibility = View.GONE
+                binding.recyclerViewComments.visibility = View.VISIBLE
+                commentAdapter.updateComments(collector.comments)
+            }
         }
 
         viewModel.eventNetworkError.observe(viewLifecycleOwner) { isNetworkError ->
             if (isNetworkError) onNetworkError()
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/app_vinilos_g17/viewmodels/CollectorDetailViewModel.kt
+++ app/src/main/java/com/example/app_vinilos_g17/viewmodels/CollectorDetailViewModel.kt
@@ -2,13 +2,10 @@ package com.example.app_vinilos_g17.viewmodels
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.app_vinilos_g17.models.Collector
+import com.example.app_vinilos_g17.models.Comment
+import com.example.app_vinilos_g17.models.Performer
 import com.example.app_vinilos_g17.repositories.CollectorsRepository
 import kotlinx.coroutines.launch
 
@@ -16,23 +13,37 @@ class CollectorDetailViewModel(application: Application, collectorId: Int) : And
 
     private val collectorsRepository = CollectorsRepository(application)
 
-    private val _collectorDetail = MutableLiveData<Collector>()
-    val collectorDetail: LiveData<Collector> get() = _collectorDetail
+    private val _collector = MutableLiveData<Collector>()
+    val collector: LiveData<Collector> get() = _collector
+
+//    private val _comments = MutableLiveData<List<Comment>>()
+//    val comments: LiveData<List<Comment>> get() = _comments
+//
+//    private val _favoritePerformers = MutableLiveData<List<Performer>>()
+//    val favoritePerformers: LiveData<List<Performer>> get() = _favoritePerformers
 
     private val _eventNetworkError = MutableLiveData(false)
     val eventNetworkError: LiveData<Boolean> get() = _eventNetworkError
+
+    private val _isNetworkErrorShown = MutableLiveData(false)
 
     init {
         fetchCollectorDetail(collectorId)
     }
 
     private fun fetchCollectorDetail(collectorId: Int) {
+        // Ejecutar la llamada en una corutina
         viewModelScope.launch {
             try {
                 // Llamada al repositorio para obtener el detalle del coleccionista
                 val collectorDetail = collectorsRepository.getCollectorDetail(collectorId)
                 Log.d("CollectorDetailViewModel", "Coleccionista obtenido: ${collectorDetail.name}")
-                _collectorDetail.postValue(collectorDetail)
+
+                // Actualizamos los LiveData con los datos del coleccionista
+                _collector.postValue(collectorDetail)
+               // _comments.postValue(collectorDetail.comments)
+               // _favoritePerformers.postValue(collectorDetail.favoritePerformers)
+
                 _eventNetworkError.value = false
             } catch (e: Exception) {
                 Log.e("NetworkError", "Error al obtener el detalle del coleccionista: ${e.message}")
@@ -41,9 +52,9 @@ class CollectorDetailViewModel(application: Application, collectorId: Int) : And
         }
     }
 
-    // Función para resetear el estado de error después de mostrarlo
+
     fun onNetworkErrorShown() {
-        _eventNetworkError.value = false
+        _isNetworkErrorShown.value = true
     }
 
     class Factory(private val app: Application, private val collectorId: Int) : ViewModelProvider.Factory {
