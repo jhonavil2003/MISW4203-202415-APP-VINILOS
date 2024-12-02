12a8bc0558d5266cb1ae14add9cc8915abc120e9
Andrea Fabiana Villamizar Ruiz <af.villamizarr1@uniandes.edu.co>
2024-12-01 22:01:10 -0500

    pruebas

--- app/src/androidTest/java/com/example/app_vinilos_g17/ui/view/CollectorDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/app_vinilos_g17/ui/view/CollectorDetailFragmentTest.kt
@@ -1,8 +1,10 @@
 package com.example.app_vinilos_g17.ui.view
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -11,6 +13,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.example.app_vinilos_g17.R
+import org.hamcrest.Matchers.allOf
+
 
 @RunWith(AndroidJUnit4::class)
 class CollectorDetailFragmentTest {
@@ -20,10 +24,18 @@ class CollectorDetailFragmentTest {
 
     @Test
     fun verifyRedirectCollectorDetail() {
-        // Hacer click en el elemento que lleva a la pantalla de detalle del coleccionista
-        //onView(withId(R.id.collectorsRv)).perform(click()) // Aquí debes usar el ID correspondiente del RecyclerView
-
-        // Verificar que el nombre del coleccionista es correcto en el detalle
-        //onView(withId(R.id.collectorName)).check(matches(withText("Poeta del pueblo"))) // Verificar que el nombre se muestra correctamente
+        Thread.sleep(1000)
+        onView(withId(R.id.navigation_collectors)).perform(ViewActions.click())
+        Thread.sleep(1000)
+        onView(withId(R.id.collectorRv)).check(matches(isDisplayed()))
+        onView(withId(R.id.collectorRv)).perform(click())
+        Thread.sleep(1000)
+        onView(allOf(withId(R.id.textView2), withText("Manolo Bellon")))
+            .perform(ViewActions.click())
+        Thread.sleep(1000)
+        onView(withId(R.id.collectorName)).check(matches(withText("Manolo Bellon")))
+        onView(withId(R.id.collectorTelephone)).check(matches(withText("3502457896")))
+        onView(withId(R.id.collectorEmail)).check(matches(withText("manollo@caracol.com.co")))
     }
+
 }
